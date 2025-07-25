e1eeab6d0d2367649dcaef412b308c1c16174163
chaser141 <chaser141@gmail.com>
2025-07-25 19:01:02 -0300

    E: Added dict create function aswell as sort function

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ def get_book_text(file_path):
 
 
 from stats import word_counter    
-    
-                    
-                       
-            
-                                                                                                        
 
+from stats import character_counter
 
-
+from stats import dict_create
 
 def main():
     contents = get_book_text("books/frankenstein.txt")
     word_count = word_counter(contents)
-    print(f"{word_count} words found in the document")
-
+    character_count = character_counter(contents)
+    end_count = dict_create(character_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("Character count")
+    for item in end_count:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,26 @@
 def word_counter(contents):
-    words = contents.split()
+    words = contents.lower().split()
     return len(words)
+
+
+
+def character_counter(contents):
+    low_words = contents.lower()
+    count = {}
+    for char in low_words:
+        count[char] = count.get(char, 0) + 1
+    return count
+
+
+def dict_create(count):
+    final_count = []
+    for char, num in count.items():
+        if char.isalpha():
+            new_dict = {"char": char, "num": num}
+            final_count.append(new_dict)
+    final_count.sort(reverse=True, key=sort_on)
+    return final_count   
+
+def sort_on(items):
+    return items["num"]
+    
