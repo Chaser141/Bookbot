c768b266b633a33311e890c7b036215835299389
chaser141 <chaser141@gmail.com>
2025-07-26 13:24:54 -0300

    F: removed hardcoded book path and added command line arg for books path

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+import sys
 
 from stats import word_counter    
 
@@ -10,13 +11,19 @@ from stats import character_counter
 
 from stats import dict_create
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+
 def main():
-    contents = get_book_text("books/frankenstein.txt")
+    contents = get_book_text(book_path)
     word_count = word_counter(contents)
     character_count = character_counter(contents)
     end_count = dict_create(character_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("Character count")
